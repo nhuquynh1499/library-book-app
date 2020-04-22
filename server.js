86c765890a1510fa459b06deb685c0660cbd6101
Glitch (hello-express) <none>
2020-04-22 04:47:16 +0000

    🚌💪 Checkpoint ./server.js:15250296/137

--- server.js
+++ server.js
@@ -6,13 +6,6 @@
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
