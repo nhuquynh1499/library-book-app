47407b3976e23d716f85456bd0aa5d9350e5470c
Glitch (hello-express) <none>
2020-04-22 07:39:15 +0000

    👍🏪 Checkpoint ./server.js:15250296/928 ./views/index.pug:15250296/105

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const low = require("lowdb");
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
 const app = express();
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+app.set('views', './views');
+app.set('view engine', 'pug');
 
+// https://expressjs.com/en/starter/basic-routing.html
 // send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
+app.get("/books", (request, response) => {
   // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+  
 });
 
 // listen for requests :)
