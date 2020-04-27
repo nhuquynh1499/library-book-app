506e6ab05a348fba939f6212137f8d20a9330d40
Glitch (feline-seemly-thing) <none>
2020-04-27 16:01:29 +0000

    🌁🌌 Checkpoint ./server.js:15250296/40 ./controllers/auth.controller.js:15250296/174 ./middlewares/auth.middleware.js:15250296/64

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,11 +4,19 @@ const saltRounds = 10;
 const md5 = require("md5");
 
 module.exports.login = (req, res) => {
+  if (!req.cookies.wrongLoginCount) {
+    res.cookie("wrongLoginCount", 0);
+  }
   res.render("auth/login");
 };
 
 module.exports.postLogin = (req, res, next) => {
-  var wrongLoginCount = 0;
+  if (parseInt(req.cookies.wrongLoginCount) > 4) {
+    res.render("auth/login", {
+      errors: ["You logged in wrongly too many times"],
+    });
+    return;
+  }
   var email = req.body.email;
   var password = req.body.password;
   var user = db
@@ -23,25 +31,17 @@ module.exports.postLogin = (req, res, next) => {
     });
     return;
   }
-  
-  if (!req.cookies.wrongLoginCount) {
-    res.cookie("wrongLoginCount", 0);
-  }
 
   var result = bcrypt.compareSync(req.body.password, user.password);
   if (!result) {
     var wrongLoginCount = parseInt(req.cookies.wrongLoginCount);
-    console.log(req.cookies.wrongLoginCount);
-    console.log(parseInt(req.cookies.wrongLoginCount));
     wrongLoginCount++;
-    console.log(wrongLoginCount);
     res.cookie("wrongLoginCount", wrongLoginCount);
     res.render("auth/login", {
       errors: ["Wrong password."],
       value: req.body
     });
   }
-  res.cookie("wrongLoginCount", wrongLoginCount);
   res.cookie("userId", user.id);
   res.redirect("/transactions");
 };
