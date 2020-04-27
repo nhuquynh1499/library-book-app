3a71ce55db5070458975f85be993afa638dde01e
Glitch (feline-seemly-thing) <none>
2020-04-27 15:40:06 +0000

    🎬⛸ Checkpoint ./controllers/auth.controller.js:15250296/2278

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,37 +1,47 @@
-const db = require('../db');
-const bcrypt = require('bcrypt');
+const db = require("../db");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const md5 = require('md5');
+const md5 = require("md5");
 
 module.exports.login = (req, res) => {
-  res.render('auth/login');
-}
+  res.render("auth/login");
+};
 
 module.exports.postLogin = (req, res, next) => {
+  var wrongLoginCount = 0;
   var email = req.body.email;
   var password = req.body.password;
-  var user = db.get('users').find({email: email}).value();
-  
+  var user = db
+    .get("users")
+    .find({ email: email })
+    .value();
+
   if (!user) {
-    res.render('auth/login', {
-      errors: [
-        'User is not exist'
-      ],
+    res.render("auth/login", {
+      errors: ["User is not exist"],
       value: req.body
-    })
+    });
     return;
   }
   
-  var result = bcrypt.compareSync(req.body.password, user.password); 
-        if (!result) {
-          res.render('auth/login', {
-            errors: [
-              'Wrong password.'
-            ],
-            value: req.body
-          })
-        }
-  
-  res.cookie('userId', user.id);
-  res.redirect('/transactions');
-}
+  if (!req.cookies.wrongLoginCount) {
+    res.cookie("wrongLoginCount", 0);
+  }
+
+  var result = bcrypt.compareSync(req.body.password, user.password);
+  if (!result) {
+    var wrongLoginCount = parseInt(req.cookies.wrongLoginCount);
+    console.log(req.cookies.wrongLoginCount);
+    console.log(parseInt(req.cookies.wrongLoginCount));
+    wrongLoginCount++;
+    console.log(wrongLoginCount);
+    res.cookie("wrongLoginCount", wrongLoginCount);
+    res.render("auth/login", {
+      errors: ["Wrong password."],
+      value: req.body
+    });
+  }
+  res.cookie("wrongLoginCount", wrongLoginCount);
+  res.cookie("userId", user.id);
+  res.redirect("/transactions");
+};
