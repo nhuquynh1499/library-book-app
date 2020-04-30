582c265e123243cf59ae7fa7a829077d75df7e3c
Glitch (feline-seemly-thing) <none>
2020-04-30 08:51:51 +0000

    👋🌉 Checkpoint ./routes/user.route.js:15250296/1264 ./controllers/user.controller.js:15250296/128 ./validate/user.validate.js:15250296/117

--- validate/user.validate.js
+++ validate/user.validate.js
@@ -3,8 +3,10 @@ module.exports.postCreate = (req, res)=> {
   var error = '';
   if (name.length > 30) {
     error = 'Name must be less than 30 characters'
-  }
-  res.render('users/create', {
+    res.render('users/create', {
     error: error
   })
+    return;
+  }
+  
 }
