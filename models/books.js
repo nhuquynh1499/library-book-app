06fb5bb6b7d4b1e788bf4583debd9d64789b1263
Glitch (feline-seemly-thing) <none>
2020-05-05 14:11:14 +0000

    🔔🎑 Checkpoint ./models/books.js:15250296/563

---
+++ models/books.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+var bookSchema = new mongoose.Schema({
+  id: String,
+  image: String,
+  title: String,
+      "description": "Doraemon is the name of a robot cat that came from the future to help a boy named Nobita Nobi. Doraemon is about the life of
+})
+
