6d435d05a54d366c576470be7624a41a458236f0
Glitch (feline-seemly-thing) <none>
2020-05-05 14:59:14 +0000

    🚟🍮 Checkpoint ./controllers/book.controller.js:15250296/2 ./DB/connection.js:15250296/167

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const URL = "mongodb+srv://" + process.env.mongodb_username + ":" + process.env.mongodb_password + "@learnexpresscodersx-kuqyf.gcp.mongodb.net/test?retryWrites=true&w=majority"
 
 const connect = async () => {
-  await mongoose.connect(URL);
+  await mongoose.connect(URL,
+    { 
+      useUnifiedTopology: true,
+      useNewUrlParser: true 
+    }
+  )
 }
 
 module.exports = connect;
