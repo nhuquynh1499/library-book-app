1dd4fc5b5c856df67b1fa0c445369f95253e1ab1
Glitch (feline-seemly-thing) <none>
2020-05-06 09:50:46 +0000

    ⚾️😻 Checkpoint ./DB/connection.js:15250296/45

--- DB/connection.js
+++ DB/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const URL = "mongodb+srv://ngngocnhuquynh0104:CodersX1499@learnexpresscodersx-kuqyf.gcp.mongodb.net/test?retryWrites=true&w=majority"
+const URL = "mongodb+srv://ngngocnhuquynh0104:CodersX1499@learnexpresscodersx-kuqyf.gcp.mongodb.net/library?retryWrites=true&w=majority/"
 
 const connect = async () => {
   await mongoose.connect(URL,
-    { 
-      useUnifiedTopology: true,
+    {
       useNewUrlParser: true 
     }
   )
