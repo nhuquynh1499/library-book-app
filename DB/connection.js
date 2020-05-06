bf817f69f200336782053639277ef56a23de2f36
Glitch (feline-seemly-thing) <none>
2020-05-06 17:11:07 +0000

    🚎😺 Checkpoint ./DB/connection.js:15250296/240

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URL = "mongodb+srv://ngngocnhuquynh0104:coderxawesome@cluster0-kuqyf.mongodb.net/library?retryWrites=true&w=majority"
+const URL = "mongodb+srv://" + process.env.mongodb_username + ":" + process.env.mongodb_password +"@cluster0-kuqyf.mongodb.net/library?retryWrites=true&w=majority"
 
 const connect = async () => {
   await mongoose.connect(URL,
