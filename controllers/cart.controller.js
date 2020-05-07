7f917298e5d20f22a69ad3ee45c83ef1355024c7
Glitch (feline-seemly-thing) <none>
2020-05-07 14:09:44 +0000

    🍨🌛 Checkpoint ./controllers/cart.controller.js:15250296/243

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const sessionModel = require('../models/sessions');
 const userModel = require('../models/users')
 const bookModel = require('../models/books')
+const transactionModel = require('../models/transactions');
 
 module.exports.index = async (req, res, next) => {
   var sessionId = req.signedCookies.sessionId;
@@ -33,7 +34,8 @@ module.exports.addToCart = async (req, res, next) => {
   //   .value();
   var session = await sessionModel.findOne({ cookieId: sessionId });
   var count = (bookId in session.cart) ? session.cart.bookId + 1 : 0;
-  await sessionModel.update({ cookieId: sessionId }, { cart.bookId: count }).exec();
+  var item = [].push({ $bookId: count})
+  await sessionModel.updateOne({ cookieId: sessionId }, { 'cart.bookId': count }).exec();
 
   // var sessions = db.get('sessions')
   //   .find({ id: sessionId })
@@ -43,18 +45,16 @@ module.exports.addToCart = async (req, res, next) => {
   res.redirect('/books');
 }
 
-module.exports.addTransaction = (req, res, next) => {
+module.exports.addTransaction = async (req, res, next) => {
   var q = req.query.q;
   var listBookId = q.split(' ');
   for (var bookId of listBookId) {
-    console.log(bookId);
     var id = shortId.generate();
-    db.get('transactions').push({
-      id: id,
+    await transactionModel.create({
       userId: req.signedCookies.userId,
       bookId: bookId,
       isComplete: false
-    }).write();
+    }).exec();
     // db.get("sessions")
     // .remove({ id: req.signedCookies.sessionId})
     // .write();
