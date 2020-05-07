cdfaadf2846acee51d0f5391bd52242cbad07c13
Glitch (feline-seemly-thing) <none>
2020-05-07 14:56:42 +0000

    ⛈🌈 Checkpoint ./middlewares/session.middleware.js:15250296/26 ./controllers/cart.controller.js:15250296/67

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -34,7 +34,11 @@ module.exports.addToCart = async (req, res, next) => {
   //   .value();
   var session = await sessionModel.findOne({ cookieId: sessionId });
   console.log(session);
-  var count = (bookId in session.cart) ? session.cart.bookId + 1 : 0;
+  if (session.cart) {
+    var count = (bookId in session.cart) ? session.cart.bookId + 1 : 0;
+  } else {
+    count = 0
+  }
   await sessionModel.updateOne({ cookieId: sessionId }, { 'cart.bookId': count }).exec();
 
   // var sessions = db.get('sessions')
