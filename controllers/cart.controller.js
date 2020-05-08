aa800fb7f6f3ee891ae1a75a7d94e86f569ece5f
Glitch (feline-seemly-thing) <none>
2020-05-08 16:53:09 +0000

    🕶👛 Checkpoint ./controllers/cart.controller.js:15250296/227

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -33,10 +33,13 @@ module.exports.addToCart = async (req, res, next) => {
   //   .get('cart.' + bookId, 0)
   //   .value();
   var session = await sessionModel.findOne({ cookieId: sessionId });
-  console.log(session);
-  var cart = session.cart;
-  if (cart) {
-    var numberBook = "cart." + bookId;
+  var count = 0;
+  if (session.cart) {
+    for (let book of session.cart.book) {
+      if (book === bookId) {
+        
+      }
+    }
     console.log(numberBook);
     var count = (bookId in cart) ? numberBook + 1 : 1;
   } else {
