af36a864b39c1c68896c32364d3db90521fffe4b
Glitch (feline-seemly-thing) <none>
2020-05-08 16:41:12 +0000

    🍨💸 Checkpoint ./models/sessions.js:15250296/253

--- models/sessions.js
+++ models/sessions.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var sessionSchema = new mongoose.Schema({
+var sessionSchema = new Schema({
   cookieId: String,
-  cart: Object
+  cart: {
+    book: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
+    quantity: Number
+  }
 })
 
 module.exports = mongoose.model('Session', sessionSchema)
