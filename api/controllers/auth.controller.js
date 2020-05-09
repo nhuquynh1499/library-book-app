3447555d3df5bc1ef54262eb22ba3b660646fbe8
Glitch (feline-seemly-thing) <none>
2020-05-09 09:02:05 +0000

    🍣👟 Checkpoint ./api/controllers/auth.controller.js:15250296/747

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,34 +1,9 @@
-const 
+const userModel = require('../../models/users');
 
 module.exports.login = async (req, res, next) => {
   var email = req.body.email;
   var password = req.body.password;
-  var user = await userModel.findOne({ email: email });
-
-  if (!user) {
-    res.render("auth/login", {
-      errors: ["User is not exist"],
-      value: req.body
-    });
-    return;
-  }
+  var user = await userModel.findOne({ email: email, password: password});
   
-  console.log(user.password);
-
-  var result = bcrypt.compareSync(req.body.password, user.password);
-  console.log(result);
-  if (!result) {
-    var wrongLoginCount = parseInt(req.cookies.wrongLoginCount);
-    wrongLoginCount++;
-    res.cookie("wrongLoginCount", wrongLoginCount);
-    res.render("auth/login", {
-      errors: ["Wrong password."],
-      value: req.body
-    });
-  }
-  res.cookie("userId", user.id, {
-    signed: true
-  });
-  res.cookie("wrongLoginCount", 0);
-  res.redirect("/transactions");
+  res.json(user);
 }
